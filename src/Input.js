c5f2a2270fa9531ca0313aa45063bfb7cf4f1998
Sagi Shutrit <contact@sagishutrit.com>
2017-08-24 11:39:06 +0200

    cleaned components

--- src/Input.js
+++ src/Input.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 
-
 export const Input = ({search}) =>{
 	
-		
 		return (<input type="text"  onChange={search.bind(this)}/>)
-	
-	
 }
--- src/People.js
+++ src/People.js
@@ -4,8 +4,7 @@ export const People = ({people}) =>{
 
 		return (<div>{
               	people.map((people,index)=>{
-     
-              		 return <p key={index}>name:{people.name} company:{people.company}, city:{people.city}, 
+     				return <p key={index}>name:{people.name} company:{people.company}, city:{people.city}, 
                  	 email:{people.email}, job_hisotry:{people.job_history}</p>
                    })
               	 }
