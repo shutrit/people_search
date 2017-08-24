5f770c149ad4f6008437491db89ef45033176a85
Sagi Shutrit <contact@sagishutrit.com>
2017-08-24 11:34:14 +0200

    refactored searchPerson

--- src/App.js
+++ src/App.js
@@ -10,37 +10,20 @@ class App extends Component {
     super(props)
     this.state = {
       people:text,
-
-     }
+    }
     this.originalState = this.state.people
   }
 
- 
   searchPerson(event){
 
-    console.log('the value:',event.target.value)
-    // 
     let b = event.target.value 
 
-
-    const g =  this.state.people.filter(function(val){
-      return val.name === b || val.company === b || val.city === b || val.email === b || val.job_history[0] === b
+    const g =  this.state.people.filter((val)=>{
+      return val.name === b || val.company === b || val.city === b || val.email === b 
     })
 
-    console.log(g.length)
- 
-    if(g.length>0) {
-      this.setState({
-        people:g
-      })
-    } else {
-      console.log("went out of range")
-      this.setState({
-        people:this.originalState,
-      })
-    }
+    g.length>0 ? this.setState({ people:g }):this.setState({ people:this.originalState })
     
-
   }
 
   render() {
@@ -49,14 +32,11 @@ class App extends Component {
       <div className="App">
          <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-       
-           </div><Input search={this.searchPerson.bind(this)}/>
-            <div className="people"><People people={this.state.people}/></div>
-
-
-
-
-   
+          </div>
+            <Input search={this.searchPerson.bind(this)}/>
+            <div className="people">
+            <People people={this.state.people}/>
+            </div>
       </div>
     );
   }
