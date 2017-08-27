106d73eabbce5b405442561c6e9f7b5c4f606f5d
Sagi Shutrit <contact@sagishutrit.com>
2017-08-27 13:39:41 +0200

    added person component

--- src/People.js
+++ src/People.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-export const People = ({people}) =>{
+export const People = ({people}) => {
 
-		return (<div>{
-              	people.map((people,index)=>{
-     				return <p key={index}>name:{people.name} company:{people.company}, city:{people.city}, 
-                 	 email:{people.email}, job_hisotry:{people.job_history}</p>
-                   })
-              	 }
-			</div>)
+	return (<div>
+		{ people.map(function(shuki,idx){
+			return <Person  key={idx} person={shuki}/>
+			}) 
+		 }</div>)
+	
+}
+
+export const Person = ({person}) => {
+
+ return (<div>
+ 	{Object.keys(person).map(function(key,index) {
+   		 return <span key={index}>{key}:{person[key]} </span>;
+		})
+	}</div>)
 }
